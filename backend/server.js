f5ff524809656fed0276cd9f26e4cbc88485e08c
Sharath Heralagi <106471249+Sharatheralagi007@users.noreply.github.com>
2025-01-07 19:33:15 +0530

    Updated the server.js for deployment

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const colors = require("colors");
 const { Server } = require("socket.io");
+const path = require("path");
 
 dotenv.config();
 connectDB();
@@ -16,10 +17,25 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:3000", methods: ["GET", "POST"] }));
 
-app.get("/", (req, res) => res.send("API is running..."));
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+// ------------------Deployment----------------------
+
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build"))); // Correct path to the build folder
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => res.send("API is running..."));
+}
+
+// ------------------Deployment----------------------
+
 app.use(notFound);
 app.use(errorHandler);
 
